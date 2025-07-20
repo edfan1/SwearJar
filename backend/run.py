b332be3dd8f0c85c989e5a631b6b7332b91200bf
edfan1 <91496311+edfan1@users.noreply.github.com>
2025-07-19 22:46:18 -0400

    Remapped ports (8000)

--- backend/run.py
+++ backend/run.py
@@ -8,4 +8,4 @@ with app.app_context():
     print("Database tables created!")
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=8000)
